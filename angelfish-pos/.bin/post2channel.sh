935710274bdb447ca53585274ab58393721165d7
rofiq <muhammad.rofiq@bhinneka.com>
2018-04-27 15:16:24 +0700

    add publish shortcut

---
+++ angelfish-pos/.bin/post2channel.sh
@@ -0,0 +1,1 @@
+curl -X POST --data-urlencode "payload={\"username\": \"buildbot\", \"text\": \"New AngelFish POS app pushed to Expo\"}" https://hooks.slack.com/services/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
