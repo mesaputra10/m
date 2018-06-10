894bad7fe66d7e2c91a0df2df06b6ab7283e8c3e
Adi Saputra <adisaputra.id@gmail.com>
2018-06-11 06:04:56 +0700

    unit test python

--- tests.py
+++ tests.py
@@ -1,1 +1,12 @@
-print ("Hello world, travis")
+import unittest
+
+import awesome
+
+
+class TestMethods(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(awesome.smile(), ":)")
+
+
+if __name__ == '__main__':
+    unittest.main()
